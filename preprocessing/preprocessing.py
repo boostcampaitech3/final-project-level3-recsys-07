ecc74c129e0d405f18643258b73dba0d960cd89a
moon <0917jong@gmail.com>
2022-05-27 01:57:20 +0000

    [FEAT][유종문] 색 preprocess

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -2,15 +2,12 @@ import numpy as np
 import pandas as pd
 from class_preprocess import class_preprocess
 from likes_preprocess import likes_preprocess
+from color_preprocess import color_preprocess
 
 raw_data = pd.read_excel("/opt/ml/input/data/raw_codishop/view/item/item.xlsx")
 
-for i in range(5) :
-    print(raw_data.iloc[i])
-
 preprocessed_data = class_preprocess(raw_data)
 preprocessed_data = likes_preprocess(preprocessed_data)
+preprocessed_data = color_preprocess(preprocessed_data)
 
-for i in range(5):
-    print(preprocessed_data.iloc[i])
 
