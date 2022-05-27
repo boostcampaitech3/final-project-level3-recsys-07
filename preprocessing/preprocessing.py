868e3b9bf3e43ad65472526c4707249635da0415
hyundukseo <hyundukseo98@gmail.com>
2022-05-27 07:19:04 +0000

    [FEAT][서현덕] 전처리 코드 수정

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -7,8 +7,15 @@ from rating_preprocess import rating_preprocess
 
 raw_data = pd.read_excel("/opt/ml/input/data/raw_codishop/view/item/item.xlsx")
 
+print("Starting Data Preprocessing!")
+print("1. Preprocessing for class...")
 preprocessed_data, need_revision_data = class_preprocess(raw_data)
+print("2. Preprocessing for likes...")
 preprocessed_data = likes_preprocess(preprocessed_data)
+print("3. Preprocessing for color...")
 preprocessed_data = color_preprocess(preprocessed_data)
+print("4. Preprocessing for rating")
 preprocessed_data = rating_preprocess(preprocessed_data)
+print("Preprocess Finished!")
 
+print(preprocessed_data.head(10))
