5499f773928ff42f76c78001b0d512a3d5e49aef
somi198 <somi198@naver.com>
2022-06-02 03:32:41 +0000

    [DOCS][김소미] 불필요한 파일 제거

--- preprocessing/rating_preprocess.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# 평점 결측치 처리
-def rating_preprocess(item_df: pd.DataFrame) -> pd.DataFrame:
-    avg_rating = item_df[item_df['rating'].notnull()]['rating'].mean()
-    item_df['rating'] = item_df['rating'].fillna(avg_rating)
-    return item_df
